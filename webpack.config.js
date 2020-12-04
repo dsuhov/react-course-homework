f5e2f6d7a2a1dee7e808c6c42d38b0c457705a30
Dmitriy Sukhov <dstujev@gmail.com>
2020-12-04 21:42:38 +0300

    added linting and base react setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    alias: {
+      types: path.resolve(__dirname, "src/types"),
+    },
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: require.resolve("babel-loader"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
