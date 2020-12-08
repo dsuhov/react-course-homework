4105b0c68fcb496a5aa607a99cdc1c8cd74da318
dsuhov <dstujev@gmail.com>
2020-12-08 14:53:36 +0300

    added emotion

--- src/Components/Incrementer/Incrementer.stories.tsx
+++ src/Components/Incrementer/Incrementer.stories.tsx
@@ -2,14 +2,15 @@ import { Meta, Story } from "@storybook/react/types-6-0";
 import React from "react";
 import { Incrementer } from "./Incrementer";
 
-
 export default {
   title: "TryIncrementor",
   component: Incrementer,
-  argTypes: { onClick: { action: 'clicked' } },
+  argTypes: { onClick: { action: "clicked" } },
 } as Meta;
 
-const Template: Story<{ color: string, onClick: any }> = (args) => <Incrementer {...args} />;
+const Template: Story<{ color: string; onClick: any }> = (args) => (
+  <Incrementer {...args} />
+);
 
 export const TryIncrementor = Template.bind({});
 
--- src/Components/Incrementer/Incrementer.tsx
+++ src/Components/Incrementer/Incrementer.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { MyButton } from "./style";
 
-export const Incrementer: React.FC<{ color?: string, onClick: any }> = ({ color, onClick }) => {
+export const Incrementer: React.FC<{ color?: string; onClick: any }> = ({
+  color,
+  onClick,
+}) => {
   return (
     <>
-      <MyButton
-        onClick={onClick}
-      >
-        Push
-      </MyButton>
+      <MyButton onClick={onClick}>Push</MyButton>
       <h1>Click</h1>
     </>
   );
